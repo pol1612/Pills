d1c084225b5086b67030a18eaaae2afeb4a022f3
Pol Sañé Jové <pol.sane@uvic.cat>
2023-07-14 21:43:07 +0200

    initial commit, added Day class

---
+++ app/src/main/java/personal/pol/sanejove/pills/Day.java
@@ -0,0 +1,48 @@
+package personal.pol.sanejove.pills;
+
+import java.util.Date;
+
+public class Day {
+    private Date Day;
+    private Boolean ThirtyGramsPill;
+    private Boolean TwentyGramsLiquid;
+    private Boolean TenGramsPill;
+
+    public Day (){
+        Day = new Date();
+        ThirtyGramsPill = false;
+        TwentyGramsLiquid = false;
+        TenGramsPill = false;
+    }
+    public Date getDay() {
+        return Day;
+    }
+
+    public void setDay(Date day) {
+        Day = day;
+    }
+
+    public Boolean getThirtyGramsPill() {
+        return ThirtyGramsPill;
+    }
+
+    public void setThirtyGramsPill(Boolean thirtyGramsPill) {
+        ThirtyGramsPill = thirtyGramsPill;
+    }
+
+    public Boolean getTwentyGramsLiquid() {
+        return TwentyGramsLiquid;
+    }
+
+    public void setTwentyGramsLiquid(Boolean twentyGramsLiquid) {
+        TwentyGramsLiquid = twentyGramsLiquid;
+    }
+
+    public Boolean getTenGramsPill() {
+        return TenGramsPill;
+    }
+
+    public void setTenGramsPill(Boolean tenGramsPill) {
+        TenGramsPill = tenGramsPill;
+    }
+}
